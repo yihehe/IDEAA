224eb9a2d2dac890e38f231cae8638a972734a01
Matthew Toso <matoso@amazon.com>
2015-08-08 14:22:31 -0700

    Hardcoded map launching

--- app/src/main/java/com/takaitra/hello/ideaa/MainActivity.java
+++ app/src/main/java/com/takaitra/hello/ideaa/MainActivity.java
@@ -19,6 +19,7 @@ package com.takaitra.hello.ideaa;
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 
@@ -29,6 +30,8 @@ import cyanogenmod.app.CMStatusBarManager;
 import cyanogenmod.app.CustomTile;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Example sample activity to publish a tile with a toggle state
@@ -42,6 +45,12 @@ public class MainActivity extends Activity implements View.OnClickListener {
     public static final String ACTION_TOGGLE_STATE =
             "org.cyanogenmod.samples.customtiles.ACTION_TOGGLE_STATE";
     public static final String STATE = "state";
+
+
+    private static final String ADDRESS_HOME = "9425 34th Ave SW, Seattle, WA 98126";
+    private static final String ADDRESS_WORK = "2201 6th Ave, Seattle, WA 98121";
+    private static final String ADDRESS_IMPACT_HUB = "220 2nd Ave S, Seattle, WA 98104";
+    private static Map<String, String> addresses = new LinkedHashMap<>();
 
     private Button mCustomTileButton;
     private Button mCustomTileButtonExpandedStyleList;
@@ -63,6 +72,10 @@ public class MainActivity extends Activity implements View.OnClickListener {
         mCustomTileButtonExpandedStyleGrid =
                 (Button) findViewById(R.id.custom_tile_grid_expanded_button);
         mCustomTileButtonExpandedStyleGrid.setOnClickListener(this);
+
+        addresses.put("Home", "9425 34th Ave SW, Seattle, WA 98126");
+        addresses.put("Work", "2201 6th Ave, Seattle, WA 98121");
+        addresses.put("Impact Hub", "220 2nd Ave S, Seattle, WA 98104");
     }
 
     @Override
@@ -89,14 +102,20 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 break;
             case R.id.custom_tile_list_expanded_button:
                 ArrayList<CustomTile.ExpandedListItem> expandedListItems =
-                        new ArrayList<CustomTile.ExpandedListItem>();
-                for (int i = 0; i < 100; i++) {
+                        new ArrayList<>();
+                for (Map.Entry<String, String> address : addresses.entrySet()) {
+                    String uri = String.format("http://maps.google.com/maps?daddr=%s", address.getValue());
+                    Intent addressIntent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(uri));
+                    addressIntent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
+                    PendingIntent addressPendingIntent = PendingIntent.getActivity(this, 0, addressIntent, PendingIntent.FLAG_ONE_SHOT);
+
                     CustomTile.ExpandedListItem expandedListItem = new CustomTile.ExpandedListItem();
                     expandedListItem.setExpandedListItemDrawable(R.drawable.ic_launcher);
-                    expandedListItem.setExpandedListItemTitle("Test: " + i);
-                    expandedListItem.setExpandedListItemSummary("Test item summary " + i);
-                    expandedListItem.setExpandedListItemOnClickIntent(pendingIntent);
+                    expandedListItem.setExpandedListItemTitle(address.getKey());
+                    expandedListItem.setExpandedListItemSummary(address.getValue());
+                    expandedListItem.setExpandedListItemOnClickIntent(addressPendingIntent);
                     expandedListItems.add(expandedListItem);
+
                 }
 
                 CustomTile.ListExpandedStyle listExpandedStyle = new CustomTile.ListExpandedStyle();
@@ -105,7 +124,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 mCustomTile = new CustomTile.Builder(this)
                         .setExpandedStyle(listExpandedStyle)
                         .setContentDescription("Generic content description")
-                        .setLabel("CustomTile Expanded List")
+                        .setLabel("Quick Directions")
                         .setIcon(R.drawable.ic_launcher)
                         .build();
                 CMStatusBarManager.getInstance(this)
